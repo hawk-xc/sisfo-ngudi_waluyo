162d5180c817cc233c32509a5da72afd6b6f7b1c
hawkxc <hawkxc068@gmail.com>
2025-03-18 21:12:48 +0700

    update : giziModel

--- app/Models/Gizi.php
+++ app/Models/Gizi.php
@@ -9,4 +9,18 @@ class Gizi extends Model
 {
     /** @use HasFactory<\Database\Factories\GiziFactory> */
     use HasFactory;
+
+    protected $table = 'gizi';
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'jenis_gizi',
+        'jenis_gizi',
+        'bahan_makanan',
+        'berat',
+        'urt',
+        'harga',
+        'keterangan',
+        'gambar'
+    ];
 }
