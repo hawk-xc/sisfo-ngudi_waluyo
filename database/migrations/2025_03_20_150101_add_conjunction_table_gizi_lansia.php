58289695c01484fc498a95b8761d31b7e7236902
hawkxc <hawkxc068@gmail.com>
2025-03-21 21:22:06 +0700

    add : add conjunction table gizi_lansia

---
+++ database/migrations/2025_03_20_150101_add_conjunction_table_gizi_lansia.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gizi_lansia', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('gizi_id')->constrained('gizi');
+            $table->foreignId('lansia_id')->constrained('lansias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gizi_lansia');
+    }
+};
