d7bbbd46bb625ad32b4ef9f419eef016cb966bf0
hawkxc <hawkxc068@gmail.com>
2025-03-30 21:13:36 +0700

    fix : rename title with page name

--- resources/views/Admin/Kegiatan/index.blade.php
+++ resources/views/Admin/Kegiatan/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Kegiatan Klinik Ngudi Waluyo') }}
+            {{ __('Data Kegiatan Klinik') }}
         </h2>
     </x-slot>
 
