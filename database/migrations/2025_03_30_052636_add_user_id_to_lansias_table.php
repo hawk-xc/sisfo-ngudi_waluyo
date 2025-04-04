44a33ef64e8c467f9742f9ca8f8b144a3515122d
hawkxpc <hawkxc68@gmail.com>
2025-04-04 12:16:03 +0700

    update : migration foreign key column on lansias [user_id]

--- database/migrations/2025_03_30_052636_add_user_id_to_lansias_table.php
+++ database/migrations/2025_03_30_052636_add_user_id_to_lansias_table.php
@@ -6,25 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('lansias', function (Blueprint $table) {
-            // create column user_id foreign key to users table 
             $table->unsignedBigInteger('user_id')->after('id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('lansias', function (Blueprint $table) {
-            //
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 };
