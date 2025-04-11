4acafc0cf8149189f671ba04269a3aff60bac1a9
hawkxc <hawkxc068@gmail.com>
2025-04-11 09:14:21 +0700

    update : set landing page by controller handler

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PemeriksaanController;
 use App\Http\Controllers\Admin\GiziController;
@@ -10,9 +11,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('landingpage');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
